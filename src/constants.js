003b503437e97b69b59d5ff15dd1a3523a17a158
Rashmi Rajput <rjay3261@gmail.com>
2026-08-14 21:34:10 +0530

    feat: Add application constants configuration file

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+// Map constants
+export const DEFAULT_MAP_CENTER = [28.6139, 77.2090];
+export const DEFAULT_ZOOM_LEVEL = 12;
+export const MIN_ZOOM_LEVEL = 8;
+export const MAX_ZOOM_LEVEL = 18;
+
+// API constants
+export const API_TIMEOUT = 5000;
+export const REFRESH_INTERVAL = 5000;
+
+// Bus tracking constants
+export const BUS_ICON_SIZE = 35;
+export const BUS_ICON_URL = '/icons/bus-marker.png';
